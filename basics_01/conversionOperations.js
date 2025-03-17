bd4ca6db348014b7e7fa18b8937ca50787d668f6
Bhumica Jaiswal <bhumikajaiswal622@gmail.com>
2025-03-17 13:48:04 +0000

    conversionOfDataTypes

---
+++ basics_01/conversionOperations.js
@@ -0,0 +1,18 @@
+// let score= undefined
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+//33abs-->NAN
+// true-->1 false-->0
+// let isLoggedIn=""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+//1-->true; 0-->false
+// ""-->false
+// "bhumica" -->true
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))

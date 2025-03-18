c8ac42247c78b42afe3e1ec65102ce457748b0bf
Bhumica Jaiswal <bhumikajaiswal622@gmail.com>
2025-03-18 10:12:23 +0000

    datatypesConcluded

---
+++ basics_01/datatypeSummary.js
@@ -0,0 +1,33 @@
+//JS is a dynamic lang 
+//primitive type
+//every type is mentioned in the commented section
+const score=100 //number
+const scoreValue=100.3 //number
+const isLoggedIn=false //boolean
+const  outsideTemp=null //typeof-object
+let userEmail;  //undefined
+const id = Symbol('123') //symbol
+const anotherId=Symbol('123') //symbol
+// console.log(id == anotherId)
+// console.log(id === anotherId);
+const bigNumber=23347347398543857984657834n;  //bigint
+
+// ********Reference type (non-primitive type)******
+const heroes=["tortoise","pikachu" ,"penguin"] //object
+let myobj = {  //object
+    name:"bhumica",
+    age:20,}
+
+    //functions
+
+const myFunction = function(){ //function
+    console.log("hello world");
+}
+
+console.log(typeof(userEmail))
+
+
+
+
+
+

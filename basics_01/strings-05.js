c1a76d5411867153b2084d9ae736248342834cd5
Bhumica Jaiswal <bhumikajaiswal622@gmail.com>
2025-03-18 18:34:00 +0000

    conceptsOfStrings

---
+++ basics_01/strings-05.js
@@ -0,0 +1,39 @@
+const name="Tortoise"
+const repoCount=20
+// console.log(name+repoCount+"value")
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLocaleUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+const newString = gameName.substring(0,4)
+// console.log(newString)
+const anotherString = gameName.slice(-7,4)
+// console.log(anotherString)
+
+const newstringOne = "   bhumica   "
+// console.log(newstringOne)
+// console.log(newstringOne.trim());
+
+const new1="bhumi-ca-jais-wal"
+const url="https:/bhumica.com/bhumi%20jais"
+console.log(url.replace('%20','-'));
+console.log(url.includes('bad'));
+console.log(new1.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+

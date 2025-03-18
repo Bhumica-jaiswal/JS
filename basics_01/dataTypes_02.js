da850274c29b64a5b807d631f2dce260ffebadbf
Bhumica Jaiswal <bhumikajaiswal622@gmail.com>
2025-03-18 17:20:01 +0000

    nameChange

---
+++ basics_01/dataTypes_02.js
@@ -0,0 +1,15 @@
+"use strict" //treat all js code as newer version
+// alert(3+3)  we are using nodejs,not browser
+let name="bhumica"
+let age="18"
+let isloggedIn=false
+//number->2 to the power 53
+//bigint
+//string-"" use double quotes
+//boolean--> true/false
+//undefined
+//symbol-->unique
+//object
+console.log(typeof"bhumica")
+console.log(typeof null)
+console.log(typeof undefined)

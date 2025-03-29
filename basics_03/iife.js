4a3f51824dda1bb4d49376fb56e0e56a1f52ae41
Bhumica Jaiswal <bhumikajaiswal622@gmail.com>
2025-03-29 06:03:43 +0000

    iifeBasics

---
+++ basics_03/iife.js
@@ -0,0 +1,20 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
+
+// ( (name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// } )('hitesh')
+
+(function phone(){
+    console.log("bhumica")
+})();
+//unnamed and arrow func
+
+((phone_number)=> {
+    console.log(`${phone_number}`)})
+    ('997632768')

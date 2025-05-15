88b13d14238f951af49fe7d8c9214de8a540a38c
Bhumica Jaiswal <bhumikajaiswal622@gmail.com>
2025-05-15 11:08:53 +0000

    call

---
+++ OOP_10/call.js
@@ -0,0 +1,12 @@
+function setusername(username){
+    this.username=username;
+    console.log("called")
+}
+
+function createuser(username,email,password){
+    setusername.call(this,username);
+    this.email=email;
+    this.password=password;
+}
+const chai= new createuser("chai","chai@google.com" , "456")
+console.log(chai);
